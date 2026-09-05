e96c318c578b7ebeeda90aa87234a7282881f164
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2026-09-05 23:03:34 +0530

    init: project setup

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import app from "./app.js";
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
